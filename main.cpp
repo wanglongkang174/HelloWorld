30a11a0b944a5c6a3f8b02dc1c7fbc39c22ec543
allen wang <2798898455@qq.com>
2025-07-29 17:40:50 +0800

    排序算法功能的补充

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
-void sort(int *arr,int size);
+void sort(int *arr,int size){
+	int tmp=0;
+	for(int i=0;i<size-1-i;++i){
+		for(int j=0;j<size-1-i;++j){
+			if(arr[j]>arr[j+1){
+				tmp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=tmp;
+			}
+			
+		}
+	}
+}
 int main(){
 	int arr[]={12,4,89,43,21,78};
 	int size=sizeof(arr)/sizeof(int);
